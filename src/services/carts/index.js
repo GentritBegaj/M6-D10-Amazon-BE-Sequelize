23078a82aa680efd82f5be152150c10f58181308
GentritBegaj <gentritbegaj@gmail.com>
2021-05-07 17:12:16 +0200

    fixes cloudinary upload

--- src/services/carts/index.js
+++ src/services/carts/index.js
@@ -3,28 +3,41 @@ const Cart = require("../../db").Cart;
 const Product = require("../../db").Product;
 const User = require("../../db").User;
 const Category = require("../../db").Category;
-const cart = require("../../db").Review;
 
-router
-  .route("/:userId")
-  .get(async (req, res, next) => {
-    try {
-      const cart = await Cart.findAll({
-        where: { userId: req.params.userId },
-        include: Product,
-      });
-      res.send(cart);
-    } catch (error) {
-      res.status(500).send({ message: error.message });
-    }
-  })
-  .post(async (req, res, next) => {
-    try {
-      const cart = await Cart.create(req.body);
-      res.send(cart);
-    } catch (error) {
-      res.status(500).send({ message: error.message });
-    }
-  });
+router.route("/:userId").get(async (req, res, next) => {
+  try {
+    const cart = await Cart.findAll({
+      where: { userId: req.params.userId },
+      include: [{ model: Product, include: Category }, User],
+    });
+    res.send(cart);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+router.route("/:userId/:productId").post(async (req, res, next) => {
+  try {
+    const cart = await Cart.create({
+      productId: req.params.productId,
+      userId: req.params.userId,
+    });
+    res.send(cart);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+// router
+//   .route("/:userId/:productId")
+//   .put(async (req, res, next) => {
+//   try {
+//     const modifiedCart = await Cart.update(
+//       where
+//     )
+//   } catch (error) {
+//     res.status(500).send({ message: error.message });
+//   }
+// })
 
 module.exports = router;
